c1f9da965ae35daae1a79f3e8f47c29d5357ab9f
indraseptama <indraseptama28@gmail.com>
2024-01-14 14:06:05 +0700

    menambahkan fungsi login

--- loginPage.js
+++ loginPage.js
@@ -1,1 +1,6 @@
 // Generate login page
+function login() {
+    // You can add your login logic here.
+    // For simplicity, let's just display an alert for now.
+    alert("Login button clicked!");
+}
