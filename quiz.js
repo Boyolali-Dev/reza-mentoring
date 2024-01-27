331a7eb020a816274da08e035fb52e1fc3d3b371
indraseptama <indraseptama28@gmail.com>
2024-01-27 12:09:06 +0700

    add quiz

---
+++ quiz.js
@@ -0,0 +1,98 @@
+/** 1
+ * Buatlah sebuah fungsi untuk penambahan sum(number_1, number_2)
+ * Parameter adalah number_1, number_2
+ * Output adalah penjumlahan dari kedua parameter tersebut
+ * Ex: console.log(sum(1,3)) => 4
+ */
+
+// Tulis di sini
+
+
+/** 2
+ * Buatlah sebuah fungsi untuk perkalian multiply(number_1, number_2)
+ * Parameter adalah number_1, number_2
+ * Output adalah perkalian dari kedua parameter tersebut
+ * Ex: console.log(multiply(1,3)) => 3
+ */
+
+// Tulis di sini
+
+/** 3
+ * Buatlah sebuah fungsi untuk pembagian divide(number_1, number_2)
+ * Parameter adalah number_1, number_2
+ * Output adalah pmebagian dari number_1 dibagi number_2
+ * Perhatikan jika number_2 kamu isi 0, gunakan if else
+ * Ex: console.log(divide(1,3)) => 0.3333
+ * console.log(divide(1,0)) => return "error, ndak bisa dibagi 0"
+ */
+
+// Tulis di sini
+
+/** 4
+ * Buatlah sebuah fungsi bernama greeting yang menerima satu parameter (nama) 
+ *  mengembalikan sebuah string "Hello, [nama]!"
+ * Parameter adalah nama
+ * Ex: console.log(greeting("reza")) => "Hello, reza"
+ */
+
+// Tulis di sini
+
+/** 5   
+ * Buatlah sebuah fungsi bernama cekGenapGanjil 
+ * yang menerima satu parameter (angka) dan mengembalikan string "Genap" 
+ * jika angka tersebut genap, dan "Ganjil" jika angka tersebut ganjil.
+ * Ex: console.log(cekGenapGanjil(2)) => "Genap"
+ * console.log(cekGenapGanjil(123)) => "Ganjil"
+ */
+
+// Tulis di sini
+
+/** 6   
+ * Buatlah sebuah fungsi bernama bandingkanAngka 
+ * yang menerima dua parameter (angka1 dan angka2) dan 
+ * mengembalikan string "Angka1 lebih besar" jika angka1 lebih besar dari angka2,
+ * "Angka2 lebih besar" jika angka2 lebih besar, dan "Angka sama besar" jika keduanya sama.
+ * Ex: console.log(bandingkanAngka(2, 1)) => "2 lebih besar"
+ * console.log(bandingkanAngka(2, 2)) => "Angka sama besar"
+ * console.log(bandingkanAngka(0, 666)) => "666 lebih besar"
+ */
+
+// Tulis di sini
+
+/** 7   
+ * Buatlah sebuah fungsi bernama reverseString
+ * yang menerima satu parameter (string) dan mengembalikan string
+ * tersebut dalam kebalikan urutan.
+ * Ex: console.log(reverseString("indras")) => "sardni"
+ * console.log(reverseString("reza")) => "azer"
+ */
+
+// Tulis di sini
+
+/** 8   
+ * Buatlah sebuah fungsi bernama cekTahunKabisat 
+ * yang menerima satu parameter (tahun) dan mengembalikan true 
+ * jika tahun tersebut adalah tahun kabisat, dan false jika tidak. 
+ * (Tahun kabisat adalah tahun yang habis dibagi 4, kecuali tahun yang habis dibagi 100 tetapi tidak habis dibagi 400).
+ * Ex: console.log(cekTahunKabisat(2012)) => true
+ * console.log(cekTahunKabisat(2111)) => false
+ */
+
+// Tulis di sini
+
+/** 9   
+ * Buatlah sebuah fungsi bernama hitungLuasLingkaran yang 
+ * menerima satu parameter (jari-jari) dan mengembalikan luas lingkaran dengan rumus π * r^2.
+ * Ex: console.log(hitungLuasLingkaran(7)) => 153.938040026
+ * console.log(hitungLuasLingkaran(14)) => 615.752160104
+ */
+
+// Tulis di sini
+
+/** 10   
+ * Buatlah sebuah fungsi bernama 
+ * filterAngka yang menerima satu parameter 
+ * berupa array angka dan mengembalikan array baru yang hanya berisi angka-angka ganjil dari array tersebut.
+ * Ex: console.log(filterAngkaGanjil([1,2,3,4,5,6,8])) => [1,3,5]
+ * console.log(filterAngkaGanjil([2,4,6,8,10])) => []
+ */
